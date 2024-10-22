5bd2c88adaf4747c659eddec5c91e48d5eba4a48
Sailazar <rafailov2006@gmail.com>
2024-10-23 02:05:50 +0300

    Initial commit

--- game.go
+++ game.go
@@ -10,14 +10,39 @@ const (
 var (
 	running  = true
 	bkgColor = rl.NewColor(147, 211, 196, 255)
+
+	grassSprite  rl.Texture2D
+	playerSprite rl.Texture2D
+
+	playerSrc  rl.Rectangle
+	playerDest rl.Rectangle
+
+	playerSpeed float32 = 3
 )
 
-func drawScene() {}
+func drawScene() {
+	rl.DrawTexture(grassSprite, 100, 50, rl.White)
+	rl.DrawTexturePro(playerSprite, playerSrc, playerDest, rl.NewVector2(playerDest.Width, playerDest.Height), 0, rl.White)
+}
 
-func input() {}
+func input() {
+	if rl.IsKeyDown(rl.KeyW) || rl.IsKeyDown(rl.KeyUp) {
+		playerDest.Y -= playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyS) || rl.IsKeyDown(rl.KeyDown) {
+		playerDest.Y += playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyA) || rl.IsKeyDown(rl.KeyLeft) {
+		playerDest.X -= playerSpeed
+	}
+	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
+		playerDest.X += playerSpeed
+	}
+}
 func update() {
 	running = !rl.WindowShouldClose()
 }
+
 func render() {
 	rl.BeginDrawing()
 
@@ -32,9 +57,17 @@ func init() {
 	rl.InitWindow(screenWidth, screenHeight, "Totoro")
 	rl.SetExitKey(0)
 	rl.SetTargetFPS(60)
+	grassSprite = rl.LoadTexture("res/Tilesets/Grass.png")
+	playerSprite = rl.LoadTexture("res/Characters/Basic Charakter Spritesheet.png")
+
+	playerSrc = rl.NewRectangle(0, 0, 48, 48)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
 }
 func quit() {
 	rl.CloseWindow()
+	rl.UnloadTexture(grassSprite)
+	rl.UnloadTexture(playerSprite)
 }
 
 func main() {
